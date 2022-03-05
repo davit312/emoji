72542611e96c84dbd03f28203ca63eb3f7d2292a
Davit Babayan <davi807plus@yandex.ru>
2022-03-06 02:25:09 +0400

    Test change for python 3.5 version support

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from itertools import count
 from emoji import *
 
+lineTemplate = "{:<5} => {:^30},\t len: {} | "
 count = 0
 for i in emoji:
     count += 1
     char = i[0]
-    print( f"{char:<5} => {i[1]:^30},\t len: {len(char)} | " , end="")
+    print( lineTemplate.format(char,i[1], len(char)) , end="")
     for c in char:
-        print(str(f"{ord(c):x} "),  end="")
+        print("{:x} ".format(ord(c)),  end="")
     print()
     if count % 100 == 0:
         print('----',count,'---')
